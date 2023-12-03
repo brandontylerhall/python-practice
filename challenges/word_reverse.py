c37341428ad8061f295ff0f90c3747d86e303470
Brandon Hall <bran.tyler.hall@gmail.com>
2023-12-03 10:07:53 -0600

    ezpz. I think I'm finally getting the whole .join() thing.

---
+++ challenges/word_reverse.py
@@ -0,0 +1,16 @@
+"""
+    Write a function that takes a sentence as input and returns
+    the words reversed (not the characters within each word)
+    e.g. "Hello World" -> "World Hello"
+"""
+print('Enter a sentence you wish to have reversed')
+sentence = input('> ')
+
+
+def reverse_sentence(sent):
+    list_sent = sent.split()[::-1]
+    rev_sent = ' '.join(list_sent)
+    print(rev_sent)
+
+
+reverse_sentence(sentence)
