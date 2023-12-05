3eec7183e58ed70d69bd8d6657f10f0958cf9901
Brandon Hall <bran.tyler.hall@gmail.com>
2023-12-05 15:26:54 -0600

    got almost too much help where I may not even fully grasp the concepts in the code. going to rebuild code from scratch.

--- challenges/list_filtering.py
+++ challenges/list_filtering.py
@@ -3,20 +3,21 @@
 """
 
 
-def list_filter(inp):
-    even_list = []
-    # turns input into a list because of split.() and removes commas because of
-    # .replace('thing to remove', 'thing to replace with') to be able to iterate through list
-    # furthermore, in list comprehension (which we just did) you must include the variable which will be in the new list
-    # e.g. num for num in inp = a new value which comprises "num"s.
-    # the second num is simply the iterable variable
-    num_list = [int(num) for num in inp.replace(',', '').split()]
-    for num in num_list:
-        if num % 2 == 0:
-            even_list.append(num)
-    return even_list
+def list_filter(inp, even=True):
+    """
+        turns input into a list because of split.() and removes commas because of
+        .replace('thing to remove', 'thing to replace with') to be able to iterate through list
+        furthermore, in list comprehension (which we just did) you must include the variable which will be in the new list
+        e.g. num for num in inp = a new value which comprises "num"s.
+        the second num is simply the iterable variable
+    """
+    inp_list = [int(num) for num in inp.replace(',', '').split()]
+    even_odd_list = [num for num in inp_list if num % 2 == 0] if even else [num for num in inp_list if num % 2 == 1]
+    return even_odd_list
 
 
 user_in = input('Enter a bunch of numbers, even and odd, separated by a comma (e.g. 1, 6, 2, 18...): ')
-new_list = list_filter(user_in)
+filter_opt = input('Would you like to filter your list to get the evens (yes/no): ').lower() == 'yes'
+
+new_list = list_filter(user_in, filter_opt)
 print(f'Your new and improved set of numbers with ONLY the evens is: {new_list}')
