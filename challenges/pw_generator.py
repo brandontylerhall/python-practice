be686579eb57f1b83ce5135a7d4eddbbfdd99989
Brandon Hall <bran.tyler.hall@gmail.com>
2023-12-06 16:35:22 -0600

    put my program into a function and added comments (that i hope are correct lol)

--- challenges/pw_generator.py
+++ challenges/pw_generator.py
@@ -1,7 +1,5 @@
 import random
 
-user_input = int(input('How many characters do you wish your password to be: '))
-
 pw_chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
             'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
             'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
@@ -9,5 +7,14 @@ pw_chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             '&', '*', '+']
 
 
-password = random.choices(pw_chars, k=user_input)
-print(f' new pw = {''.join(password)}')
+def pw_generator(inp):
+    # LENGTH VARIABLE MUST BE CALLED "k" OR IT WILL NOT WORK
+    # .choices takes the first argument and randomizes it, returning
+    # a new list with the same elements, including duplicates
+    password = random.choices(pw_chars, k=inp)  # choices allows user input for trimming of the list to that value
+    return ''.join(password)
+
+
+user_input = int(input('How many characters do you wish your password to be: '))
+new_pw = pw_generator(user_input)
+print(f'new pw = {new_pw}')
