cff20a655b09bfa64c4730d6235a9294f8fc88d5
Brandon Hall <bran.tyler.hall@gmail.com>
2023-12-11 12:58:08 -0600

    modulated the app to make it clearer to read and to make it create the file and immediately go into asking the user for input

---
+++ inventory_app/run_app.py
@@ -0,0 +1,43 @@
+import csv
+from pathlib import Path
+from add_item import add_item
+from remove_item import remove_item
+from show_item import show_all, show_names
+from update_item import update_item
+
+
+def run_app():
+    filename = 'inventory.csv'
+
+    while True:
+        with open(filename, 'a', newline='') as file:
+            writer = csv.writer(file)
+            # If the file is empty, write the header
+            if file.tell() == 0:
+                writer.writerow(['Name', 'Price', 'Quantity'])
+
+            choice = input('Choose an option: \n'
+                           '1. Add new item\n'
+                           '2. Remove item\n'
+                           '3. Update inventory\n'
+                           '4. Show all items\n'
+                           '5. Exit application\n'
+                           '> ')
+            match choice:
+                case '1':
+                    add_item(writer)
+                    file.flush()
+                case '2':
+                    show_names()
+                    remove_item()
+                    file.flush()
+                case '3':
+                    show_names()
+                    item_to_update = input("Enter the name of the item to update: ")
+                    update_item(item_to_update)
+                    file.flush()
+                case '4':
+                    show_all()
+                case '5':
+                    print('Til next time!')
+                    exit()
