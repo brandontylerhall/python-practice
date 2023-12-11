8434027be8aa4ebde565dc9e6f136485df18d323
Brandon Hall <bran.tyler.hall@gmail.com>
2023-12-11 11:37:26 -0600

    figured out how to ask for input and conditionally update various values dependent on the input provided

---
+++ inventory_app/update_item.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def update_item(item_name):
+    filename = 'inventory.csv'
+
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(filename)
+
+    choice = input("What would you like to update?\n"
+                   "> ").lower()
+    if choice == 'price':
+        new_price = float(input("What is the new price?\n"
+                                "> "))
+        df.loc[df['Name'] == item_name, 'Price'] = new_price
+    elif choice == 'quantity':
+        new_quantity = int(input("What is the new quantity?\n"
+                                 "> "))
+        df.loc[df['Name'] == item_name, 'Quantity'] = new_quantity
+    else:
+        print("Invalid choice. No updates were made.")
+
+    # Write the modified DataFrame back to the CSV file
+    df.to_csv(filename, index=False)
