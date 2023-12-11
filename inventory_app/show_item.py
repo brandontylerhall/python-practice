313adf054cd40f9a9c3c9982ea09d15ab330d780
Brandon Hall <bran.tyler.hall@gmail.com>
2023-12-11 12:14:30 -0600

    made it actually work. previously did not do what I intended because of the index_col=0. now, it is nested in a try/except and parses the output to do the same thing i was trying to do before with the index_col=0

--- inventory_app/show_item.py
+++ inventory_app/show_item.py
@@ -1,12 +1,17 @@
+# show_item.py
 import pandas as pd
 
-df = pd.read_csv('inventory.csv', index_col=0)
+try:
+    df = pd.read_csv('inventory.csv')
 
 
-def show_all():
-    print(df)
+    def show_all():
+        print(df.to_string(index=False))
 
 
-def show_names():
-    names_column = df['Name']
-    print(names_column)
+    def show_names():
+        names_column = df['Name']
+        print(f'Item names available:\n{names_column.to_string(index=False)}')
+
+except FileNotFoundError:
+    print("Error: 'inventory.csv' file not found.")
