fd024de9a76d3f4bd7cdf064a7ed8f74d68f93bd
Brandon Hall <bran.tyler.hall@gmail.com>
2023-12-11 12:27:20 -0600

    inadvertently broke it because the pd.read_csv was being read before the inventory file was being created

--- inventory_app/show_item.py
+++ inventory_app/show_item.py
@@ -1,17 +1,13 @@
 # show_item.py
 import pandas as pd
 
-try:
+
+def show_all():
     df = pd.read_csv('inventory.csv')
+    print(df.to_string(index=False))
 
 
-    def show_all():
-        print(df.to_string(index=False))
-
-
-    def show_names():
-        names_column = df['Name']
-        print(f'Item names available:\n{names_column.to_string(index=False)}')
-
-except FileNotFoundError:
-    print("Error: 'inventory.csv' file not found.")
+def show_names():
+    df = pd.read_csv('inventory.csv')
+    names_column = df['Name']
+    print(f'Item names available:\n{names_column.to_string(index=False)}')
