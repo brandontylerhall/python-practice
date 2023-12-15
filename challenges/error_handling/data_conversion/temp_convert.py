197c8a91f4f707a9f53854efd0e1ec69257cdb52
Brandon Hall <bran.tyler.hall@gmail.com>
2023-12-15 13:42:32 -0500

    moved to new filepath that made more sense. took off unnecessary comments and added a new case. need to make it work, but it's in its infancy

--- challenges/error_handling/data_conversion/temp_convert.py
+++ challenges/error_handling/data_conversion/temp_convert.py
@@ -1,9 +1,3 @@
-"""
-Write a program that converts temperatures between Celsius and Fahrenheit.
-Prompt the user to enter a temperature in one scale and print the converted temperature.
-"""
-
-
 def f_to_c():
     try:
         f = float(input('What is the temperature you want to convert in °F: '))
@@ -22,17 +16,21 @@ def c_to_f():
         print('That isn\'t Freedom OR a Queen Unit, brah. Try again.')
 
 
-while True:
-    print('Do you want to convert: \n'
-          '1. Freedom Units to Queen Units \n'
-          '2. Queen Units to Freedom Units \n'
-          '3. Quit like the yella-bellied coward you are')
-    user_input = int(input('> '))
-    match user_input:
-        case 1:
-            f_to_c()
-        case 2:
-            c_to_f()
-        case 3:
-            print('Good riddance, coward...')
-            quit()
+def temp_convert():
+    while True:
+        print('Do you want to convert: \n'
+              '1. Freedom Units to Queen Units \n'
+              '2. Queen Units to Freedom Units \n'
+              '3. Return to the main menu\n'
+              '4. Quit like the yella-bellied coward you are')
+        user_input = int(input('> '))
+
+        match user_input:
+            case 1:
+                f_to_c()
+            case 2:
+                c_to_f()
+            case 3:
+                return
+            case 4:
+                print('Good riddance, coward...')
